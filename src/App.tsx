51e72ea69d6251f2e66f95104ba91798a38e7dab
Meiyi <ant19950601@gmail.com>
2024-01-16 23:20:56 +1100

    :memo: add start and end time

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import DatePicker, { DatePickerProps } from 'antd/es/date-picker'
 import dayjs from 'dayjs'
-
-const dateFormat = 'YYYY/MM/DD'
+import type { Dayjs } from 'dayjs'
+import { TimePicker } from 'antd'
+import { dateFormat, format } from './constants/EnumType'
 
 function App() {
   const [selectTime, setSelectTime] = useState<TimeType>({
@@ -18,20 +19,54 @@ function App() {
     setSelectTime((prev) => ({ ...prev, Date: today }))
   }, [])
 
-  const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+  const changeDate: DatePickerProps['onChange'] = (date, dateString) => {
     console.log(date, dateString, '--', selectTime)
     setSelectTime((prev) => ({ ...prev, Date: dateString }))
   }
+
+  const changeStartTime = (time: Dayjs | null) => {
+    console.log(time)
+
+    setSelectTime((prev) => ({ ...prev, StartTime: time }))
+  }
+
+  const changeEndTime = (time: Dayjs | null) => {
+    setSelectTime((prev) => ({ ...prev, EndTime: time }))
+  }
+
   return (
     <main>
-      <span>日期</span>
-      <DatePicker
-        onChange={onChange}
-        showToday
-        value={dayjs(selectTime.Date, dateFormat)}
-        format={dateFormat}
-        allowClear={false} // 禁用清除日期
-      />
+      <div>
+        <span>日期</span>
+        <DatePicker
+          onChange={changeDate}
+          showToday
+          value={dayjs(selectTime.Date, dateFormat)}
+          format={dateFormat}
+          allowClear={false}
+        />
+      </div>
+      <div>
+        <span>開始時間</span>
+        <TimePicker
+          value={selectTime.StartTime}
+          format={format}
+          onChange={changeStartTime}
+          showNow={false}
+        />
+        <button type="button" onClick={() => changeStartTime(dayjs())}>此刻</button>
+      </div>
+      <div>
+        <span>結束時間</span>
+        <TimePicker
+          value={selectTime.EndTime}
+          format={format}
+          onChange={changeEndTime}
+          showNow={false}
+        />
+        <button type="button" onClick={() => changeEndTime(dayjs())}>此刻</button>
+      </div>
+
     </main>
   )
 }
