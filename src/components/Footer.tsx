ef0722a62843e1836a2a1d510733703db81a3eaa
Meiyi <ant19950601@gmail.com>
2024-01-25 22:35:43 +1100

    :memo: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Made by @
+        <a href="https://github.com/MeganxLi">
+          Megan Lee
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Footer from './components/Footer'
+import { Fragment } from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />,
+  <Fragment>
+    <App />
+    <Footer />
+  </Fragment>,
 )
